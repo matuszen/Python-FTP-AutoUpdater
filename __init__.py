39402307674547c85e8ad294e81e6cf00136f20b
matuszen <mateuszn641@gmail.com>
2023-02-17 08:04:11 +0100

    new config file structure

--- __init__.py
+++ __init__.py
@@ -1,76 +1,75 @@
 from utility import *
 from upload import *
 from remove import *
+from configparser import ConfigParser
+
 
 def main():
-    
-    with open(Path('./config.ini'), 'r') as file:
-        data = file.readlines()
-        file.close()
 
-    server = data[0].split('=')[1].strip()
-    login = data[1].split('=')[1].strip()
-    passwd = data[2].split('=')[1].strip()
-    port = data[3].split('=')[1].strip()
-    protocole = data[4].split('=')[1].strip()
-    tls = data[5].split('=')[1].strip()
-    atSign = data[6].split('=')[1].strip()
-    originPath = Path(data[7].split('=')[1].strip())
-    destPath = data[8].split('=')[1].strip()
-    uploadOnly = data[9].split('=')[1].strip()
-    deleteOnly = data[10].split('=')[1].strip()
-    runOnly = data[11].split('=')[1].strip()
-    isModuleInProject = data[12].split('=')[1].strip()
-    locationInProject = Path(data[13].split('=')[1].strip())
-    disabledElements = tuple(data[14].split('=')[1].strip().split(', '))
+    conf = ConfigParser()
+    conf.read('config.ini')
 
-    if atSign == 'True': atSign = True
-    elif atSign == 'False': atSign = False
+    host = str(conf['CONNECT']['host'])
+    port = int(conf['CONNECT']['port']) if conf['CONNECT']['port'] != 'default' else str(conf['CONNECT']['port'])
+    protocole = str(conf['CONNECT']['protocole'])
+    useTLS = bool(conf['CONNECT']['useTLS'] == 'True')
 
-    ftp = conn(server, login, passwd, protocole, port, tls = tls, atSign = atSign)
+    login = str(conf['USER']['login'])
+    password = str(conf['USER']['password'])
 
-    ftp.cwd(destPath)
-    chdir(originPath)
+    sourceDirection = Path(conf['DIRECTORIES']['sourceDirection'])
+    destinationDirection = str(conf['DIRECTORIES']['destinationDirection'])
 
-    if isModuleInProject == 'True':
+    programStatus = str(conf['SCRIPT SETTINGS']['programStatus'])
+    isModuleInProject = bool(conf['SCRIPT SETTINGS']['isModuleInProject'] == 'True')
+    locationInProject = Path(conf['SCRIPT SETTINGS']['locationInProject'])
+    disabledElements = tuple(conf['SCRIPT SETTINGS']['disabledElements'].split(', '))
 
-        originPath = Path(f'{originPath.cwd()}{locationInProject}')
+    ftp = connect(host, login, password, protocole, port, tls=useTLS)
 
-        with open(Path(f'{originPath}/config.ini'), 'w') as file:
+    chdir(sourceDirection)
+
+    ftp.cwd(destinationDirection)
+
+    if isModuleInProject:
+
+        sourceDirection = Path(f'{sourceDirection.cwd()}{locationInProject}')
+
+        with open(Path(f'{sourceDirection}/config.ini'), 'w') as file:
             data[7] = f'sourceDirection = {Path.cwd()}\n'
             file.writelines(data)
             file.close()
-    
+
     log(f'Source direction: {Path.cwd()}')
 
-    mainDir = str(originPath).split('\\')[-1]
+    mainDir = str(sourceDirection).split('\\')[-1]
 
     try:
         ftp.nlst().index(mainDir)
     except ValueError:
-        uploadOnly == 'True'
+        programStatus == 'test'
         ftp.mkd(mainDir)
 
-    if uploadOnly == 'False' and runOnly == 'False':
+    if programStatus == 'all' or programStatus == 'delete':
 
-        chdir(originPath)
+        chdir(sourceDirection)
 
-        ftp.cwd(f'{destPath}/{mainDir}')
+        ftp.cwd(f'{destinationDirection}/{mainDir}')
 
         log(f'Destination direction: {ftp.pwd()}')
 
         try:
             ftp = checkFileStructure(ftp, mainDir)
-            log(ftp.pwd())
         except Exception as e:
             log(e)
         else:
             log('Delete completed. No problems found')
+            log(f'Change direction to {ftp.pwd()}')
 
-    if deleteOnly == 'False' and runOnly == 'False':
+    if programStatus == 'all' or programStatus == 'upload':
 
-        chdir(originPath)
-        ftp.cwd(f'{destPath}/{mainDir}')
+        chdir(sourceDirection)
+        ftp.cwd(f'{destinationDirection}/{mainDir}')
 
         try:
             ftp = createFileStructure(ftp, disabledElements)
@@ -78,7 +77,6 @@ def main():
             log(e)
         else:
             log('Transfer completed. No problems found')
-
 
     ftp.quit()
 
--- utility.py
+++ utility.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 from ftplib import FTP, FTP_TLS
-# import pysftp
 from os import listdir, chdir, getcwd
 from pathlib import Path
 import numpy as np
+
 
 def log(messege: str, showHour: bool = True) -> None:
     """Logs messege to console
@@ -23,14 +23,14 @@ def log(messege: str, showHour: bool = True) -> None:
         print(messege)
 
 
-def conn(server: str, login: str, passwd: str, protocole: str, port: int = 'default', tls: bool = False, ssh: bool = False, atSign: bool = True) -> FTP:
-    """Starts and supports connection via FTP or SFTP protocole
+def connect(server: str, user: str, passwd: str, protocole: str, port: int | str = 'default', tls: bool = False, ssh: bool = False, atSign: bool = True) -> FTP:
+    """Starts and supports connection via FTP protocole
 
     Parameters
     ----------
     server : str
         the address of the server to which to connect
-    login : str
+    user : str
         the login of the user to which to log in
     passwd : str
         user's passord
@@ -44,27 +44,12 @@ def conn(server: str, login: str, passwd: str, protocole: str, port: int = 'defa
     Returns
     -------
     object
-        the object containing the entire connection to the server
-    """
+        the object containing the entire connection to the server"""
 
-    if server == '' or login == '':
+    if server == '' or user == '':
         log('Wrong parameters')
 
-    passwdHash = ''
-
-    for char in range(len(passwd)):
-        passwdHash += "*"
-    
-    if atSign:
-        for char in range(len(login)):
-            if char == "@":
-                user = login
-                break
-        else:
-            user = f'{login}@{server}'
-
-    else:
-        user = login
+    passwdHash = "*" * len(passwd)
 
     log('Starting connection')
     log(f'Server: {server}')
@@ -77,9 +62,9 @@ def conn(server: str, login: str, passwd: str, protocole: str, port: int = 'defa
             port = int(port)
 
         if tls == 'True':
-            session = useFTP(server, user, passwd, port, passwdHash, tls = True)
+            session = useFTP(server, user, passwd, port, passwdHash, tls=True)
         else:
-            session = useFTP(server, user, passwd, port, passwdHash, tls = False)
+            session = useFTP(server, user, passwd, port, passwdHash, tls=False)
 
     # elif protocole == 'SFTP':
 
@@ -119,7 +104,7 @@ def useFTP(server: str, login: str, passwd: str, port: int, passwdHash: str, tls
         try:
             ftp = FTP_TLS()
             ftp.connect(server, port)
-    
+
         except Exception as e:
             log(e)
 
@@ -154,98 +139,9 @@ def useFTP(server: str, login: str, passwd: str, port: int, passwdHash: str, tls
 
     except:
         log('Login or password incorrect')
-    
+
     else:
         log(f'Succesfully logged into {login}')
         log(ftp.getwelcome())
-    
+
     return ftp
-
-
-# def useSFTP(server: str, login: str, passwd: str, port: int = 'default') -> FTP:
-#     """Supports connection via SFTP protocole
-
-#     Parameters
-#     ----------
-#     server : str
-#         the address of the server to which to connect
-#     login : str
-#         the login of the user to which to log in
-#     passwd : str
-#         user's passord
-#     port : int, optional
-#         custom port to use in connection, if empty, use default port
-
-#     Returns
-#     -------
-#     FTP
-#         the object containing the entire connection to the server. Create by pysftp
-#     """
-
-#     if server == '' or login == '':
-#         log('Wrong parameters')
-#         return None
-
-#     passwdHash = ''
-
-#     for char in range(len(passwd)):
-#         passwdHash += "*"
-
-#     for char in range(len(login)):
-#         if char == "@":
-#             user = login
-#             break
-#     else:
-#         user = f'{login}@{server}'
-
-#     log('Starting connection')
-#     log(f'Server: {server}')
-
-#     if tls == 'True':
-#         try:
-#             if port != 'default':
-#                     ftp = FTP_TLS()
-#                     ftp.connect(server, int(port))
-#             else:
-#                 ftp = FTP_TLS(server)
-#         except Exception as e:
-#             log(e)
-#             return None
-#         else:
-#             log(f'Succesfully connected to {server}')
-
-#         ftp.auth()
-#         log('Protocole: FTP (TLS support)')
-#         log(f'SSL version: {ftp.ssl_version}')
-
-#     else:
-#         try:
-#             if port != 'default':
-#                 ftp = FTP()
-#                 ftp.connect(server, port)
-#             else:
-#                 ftp = FTP(server)
-#         except Exception as e:
-#             log(e)
-#             return None
-#         else:
-#             log(f'Succesfully connected to {server}')
-
-#         log('Protocole: FTP')
-
-#     log(f'Port: {ftp.port}')
-#     ftp.encoding = 'utf-8'
-#     log('Encoding: UTF-8')
-
-#     try:
-#         log(f'User: {user}')
-#         log(f'Password: {passwdHash}')
-#         ftp.login(user, passwd)
-#     except Exception as e:
-#         log('Login or password incorrect')
-#         return None
-#     else:
-#         log(f'Succesfully logged into {user}')
-#         log(ftp.getwelcome())
-    
-#     return ftp
